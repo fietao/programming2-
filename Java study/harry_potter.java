54d5d9b2c118789266b7a21a2f323b78d6a07def
fietao <104269994+fietao@users.noreply.github.com>
2026-02-10 17:02:52 -0600

    Add files via upload

---
+++ Java study/harry_potter.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class harry_potter {
+    public static void main(String args []){
+        int Grindylow = 20, Troll = 40, Basilisk = 80, Dementor = 60, Health;
+        Scanner skd = new Scanner(System.in);
+    System.out.println("Harry Potter is being attacked!");
+        System.out.print("How much health should he start with? (1-100):");
+    Health = skd.nextInt();
+    System.out.println("Which creature is attacking?");
+        System.out.print("1. Grindylow\n" + 
+                        "2. Troll\n" + 
+                        "3. Basilisk\n" + 
+                        "4. Dementor\n");
+
+    
+    
+    
+    
+    
+    
+            System.out.println(Health);
+
+    }
+
+    
+
+
+}
