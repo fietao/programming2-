f1028e5d3acc0c8e4b69acff3d093bcda3a405df
Jet home <Jetzazaver400@gmail.com>
2026-02-11 00:10:31 -0600

    what I changed

--- harry_potter.java
+++ harry_potter.java
@@ -1,28 +1,70 @@
-import java.util.Scanner;
-public class harry_potter {
-    public static void main(String args []){
-        int Grindylow = 20, Troll = 40, Basilisk = 80, Dementor = 60, Health;
-        Scanner skd = new Scanner(System.in);
-    System.out.println("Harry Potter is being attacked!");
-        System.out.print("How much health should he start with? (1-100):");
-    Health = skd.nextInt();
-    System.out.println("Which creature is attacking?");
-        System.out.print("1. Grindylow\n" + 
-                        "2. Troll\n" + 
-                        "3. Basilisk\n" + 
-                        "4. Dementor\n");
 
-    
-    
-    
-    
-    
-    
-            System.out.println(Health);
+/** 
+Name: Thanadon Chutrakoon 
+Date: 2/10/2026 
+Class Section: MW
+*/
+
+import java.util.Scanner;
+
+public class harry_potter {
+    public static void main(String[] args) {
+        Scanner skd = new Scanner(System.in);
+        char Patronus_charm;
+        int Health, Choice;
+        int Grindylow = 20, Troll = 40, Basilisk = 80, Dementor = 60;
+        System.out.println("Harry Potter is Being attacked!");
+        System.out.print("Enter your health(1-100): ");
+        Health = skd.nextInt();
+        while (Health > 100 || Health < 0) {
+            System.out.print("Invalid value enter again");
+            Health = skd.nextInt();
+        }
+        System.out.print("which creature is attacking? \n" + "1. Grindylow\n" +
+                "2. Troll\n" +
+                "3. Basilisk\n" +
+                "4. Dementor\n");
+        System.out.print("Enter you choice: ");
+        Choice = skd.nextInt();
+        switch (Choice) {
+            case 1:
+                System.out.print(
+                        "Harry is attacked by a Grindylow which deals 20 damage. \n" + "Harry ends the battle with "
+                                + (Health - Grindylow) + " Health\n");
+                Health = Health - Grindylow;
+                break;
+            case 2:
+                System.out.print(
+                        "Harry is attacked by a Troll which deals 40 damage. \n" + "Harry ends the battle with "
+                                + (Health - Troll) + " Health\n");
+                Health = Health - Troll;
+                break;
+            case 3:
+                System.out.print(
+                        "Harry is attacked by a Basilisk which deals 80 damage. \n" + "Harry ends the battle with "
+                                + (Health - Basilisk) + " Health\n");
+                Health = Health - Basilisk;
+                break;
+            case 4:
+                System.out.print("Has Harry learned the Patronus charm yet? y/n:");
+                Patronus_charm = skd.next().charAt(0);
+                if (Patronus_charm == 'Y' || Patronus_charm == 'y') {
+                    System.out.println("Harry is attacked by a Dementor which deals 60 damage.\n"
+                            + "Harry ends the battle with " + (Health - Dementor) + " health.");
+                } else {
+                    System.out.println("Harry suffers the Dementor's Kiss and... it's not pretty. Poor Harry.");
+                    Health = 0;
+                }
+
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
+
+        if (Health > 0) {
+            System.out.println("Harry is still alive");
+        }
 
     }
-
-    
-
-
 }
