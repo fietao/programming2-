87c7e75d84cb969b6f75eff36ec668749c84c62e
fietao <104269994+fietao@users.noreply.github.com>
2026-02-17 11:52:42 -0600

    Refactor health management and constants in game
    Refactor damage constants to final variables and improve health check logic.

--- harry_potter.java
+++ harry_potter.java
@@ -1,9 +1,14 @@
 
+
 /** 
 Name: Thanadon Chutrakoon 
 Date: 2/10/2026 
 Class Section: MW
 */
+
+/**
+ this game is provide the menu to play game and calculate harry health
+ */
 
 import java.util.Scanner;
 
@@ -12,7 +17,7 @@ public class harry_potter {
         Scanner skd = new Scanner(System.in);
         char Patronus_charm;
         int Health, Choice;
-        int Grindylow = 20, Troll = 40, Basilisk = 80, Dementor = 60;
+        final int Grindylow = 20, Troll = 40, Basilisk = 80, Dementor = 60;
         System.out.println("Harry Potter is Being attacked!");
         System.out.print("Enter your health(1-100): ");
         Health = skd.nextInt();
@@ -28,20 +33,17 @@ public class harry_potter {
         Choice = skd.nextInt();
         switch (Choice) {
             case 1:
-                System.out.print(
-                        "Harry is attacked by a Grindylow which deals 20 damage. \n" + "Harry ends the battle with "
+                System.out.print("Harry is attacked by a Grindylow which deals 20 damage. \n" + "Harry ends the battle with "
                                 + (Health - Grindylow) + " Health\n");
                 Health = Health - Grindylow;
                 break;
             case 2:
-                System.out.print(
-                        "Harry is attacked by a Troll which deals 40 damage. \n" + "Harry ends the battle with "
+                System.out.print("Harry is attacked by a Troll which deals 40 damage. \n" + "Harry ends the battle with "
                                 + (Health - Troll) + " Health\n");
                 Health = Health - Troll;
                 break;
             case 3:
-                System.out.print(
-                        "Harry is attacked by a Basilisk which deals 80 damage. \n" + "Harry ends the battle with "
+                System.out.print("Harry is attacked by a Basilisk which deals 80 damage. \n" + "Harry ends the battle with "
                                 + (Health - Basilisk) + " Health\n");
                 Health = Health - Basilisk;
                 break;
@@ -51,19 +53,28 @@ public class harry_potter {
                 if (Patronus_charm == 'Y' || Patronus_charm == 'y') {
                     System.out.println("Harry is attacked by a Dementor which deals 60 damage.\n"
                             + "Harry ends the battle with " + (Health - Dementor) + " health.");
+                    Health = Health - Dementor;
                 } else {
                     System.out.println("Harry suffers the Dementor's Kiss and... it's not pretty. Poor Harry.");
                     Health = 0;
                 }
 
                 break;
+                
             default:
-                System.out.println("Invalid choice");
                 break;
         }
-
-        if (Health > 0) {
-            System.out.println("Harry is still alive");
+        
+        if (Choice >= 1 && Choice <= 4) {
+            if (Health > 0) {
+                System.out.println("Harry is still alive");
+            }
+            else {
+                System.out.println("Harry has died");
+            }
+        }
+        else {
+            System.out.println("Sorry. " + Choice + " isn't a valid creature. Harry must not want to do battle today.");
         }
 
     }
