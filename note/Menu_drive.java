d4389f046e0686c2fa1b4af70ead734d707d9cfa
Jet home <Jetzazaver400@gmail.com>
2026-02-18 00:04:58 -0600

    well I try to pratice the chapter 5 unit which is confusing a bit

--- note/Menu_drive.java
+++
@@ -1,7 +0,0 @@
-public class Menu_drive {
-    public static void main(String[] args){
-        System.out.println(" Harry Potter is being attacked!" );
-        System
-
-    }
-}
--- note/unit_4.java
+++ note/unit_4.java
@@ -1,44 +1,82 @@
-import java.util.Scanner; 
-public class unit_4 
-{
-    public static void main(String[] args) {
-        char  variable; 
-        Scanner skb = new Scanner(System.in); 
-         /*System.out.print("Put a number between 5-10 ");
-        a = skb.nextInt();
-        while(a > 10 || a <  5 ){
-            System.out.println("Invalid number enter again");
-            a = skb.nextInt();
+import java.util.Scanner;
 
-        } 
-        System.out.println("valid value :" + a++); 
-        System.out.println("now enter Y or N");
-        variable = skb.nextLine().charAt(0);
-        while(variable != 'Y' && variable !='N') 
-        { System.out.println("enter invlaid variabel try again: ");
-            variable = skb.nextLine().charAt(0);
-        }
-        System.out.println("valid valueable: ");  */ 
+public class unit_4 {
 
-        for(int a = 8; a <13 ; a++)
-        {
-            System.out.println(a); 
+    /*
+     * public static void main(String[] args) {
+     * char variable;
+     * Scanner skb = new Scanner(System.in);
+     * /*System.out.print("Put a number between 5-10 ");
+     * a = skb.nextInt();
+     * while(a > 10 || a < 5 ){
+     * System.out.println("Invalid number enter again");
+     * a = skb.nextInt();
+     * 
+     * }
+     * System.out.println("valid value :" + a++);
+     * System.out.println("now enter Y or N");
+     * variable = skb.nextLine().charAt(0);
+     * while(variable != 'Y' && variable !='N')
+     * { System.out.println("enter invlaid variabel try again: ");
+     * variable = skb.nextLine().charAt(0);
+     * }
+     * System.out.println("valid valueable: ");
+     */
+
+    /*
+     * for(
+     * int a = 8;a<13;a++)
+     * {
+     * System.out.println(a);
+     * }for(
+     * int H = 10;H>0;H--)
+     * {
+     * System.out.print(H + " ");
+     * }System.err.println(" ");for(
+     * int L = 100;L>-1;L-=7)
+     * {
+     * System.out.print(L + " ");
+     * }System.out.println(" ");for(
+     * int K = 100;K>0;K--)
+     * {
+     * if (K % 6 == 0 || K % 8 == 0)
+     * System.out.print(K + " ");
+     * }
+     * }
+     * 
+     * public static voide main(string main[]){
+     * 
+     * 
+     * 
+     * }
+     */
+    /**
+     * @param args
+     */
+    public static int findlargest(int x, int y, int z) {
+        int largest = x;
+
+        if (y > largest) {
+            largest = y;
         }
-         for(int H =10; H > 0 ; H--)
-        {
-            System.out.print(H + " "); 
+        if (z > largest) {
+            largest = z;
         }
-        System.err.println(" ");
-        for(int L = 100; L >-1 ; L -= 7)
-        {
-        System.out.print(L + " ");
-        }
-        System.out.println(" "); 
-        for(int K = 100; K > 0  ; K--)
-        { if(K % 6 == 0 || K % 8 == 0)
-            System.out.print(K + " "); 
-        }
+        return largest;
     }
 
-    
+    public static void main(String[] args) {
+        Scanner skb = new Scanner(System.in);
+        System.out.println("enter the 3 value and they will show the biggest one");
+        int x = skb.nextInt();
+        int y = skb.nextInt();
+        int z = skb.nextInt();
+
+        if (x == y && y == z) {
+            System.out.println("all the three value are equal");
+        } else {
+            int largest = findlargest(x, y, z);
+            System.out.println(largest + " the largest ");
+        }
+    }
 }
