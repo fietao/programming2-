89bd48443ec63fa703ae00e2cb0d2be25869e877
Jet <jetzazaver400@gmail.com>
2026-02-18 09:44:13 -0600

    add the unit 7 study

---
+++ Unit7.java
@@ -0,0 +1,16 @@
+public class Unit7
+import java.util.Scanner;
+import java.util.Arrays;
+
+{
+    public static void main(String [] args)
+{
+    System.out.println("This unit the concept is all about array");
+    System.out.println("array is a  data that primitive(stores simple value directly) types it is referrence data");
+    System.out.println("array is requird to store the \r\n"); 
+    System.out.println("3multiple value of the same type in a single variable"); 
+    double [] payrate = new double[25]; 
+    //this code doube array referenced by the variable stored 25
+    
+}
+}
